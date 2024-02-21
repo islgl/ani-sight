21f98e45d1f7bce06e17c5364fda848b6f43a3b3
lgl <hi@lglgl.cc>
2024-02-21 13:31:58 +0800

    update cache config

--- src/main/java/cc/lglgl/anisight/config/CacheConfig.java
+++ src/main/java/cc/lglgl/anisight/config/CacheConfig.java
@@ -10,54 +10,56 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ScanOptions;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import java.util.HashSet;
-import java.util.Set;
-
 @Configuration
 @EnableCaching
 public class CacheConfig {
-    @Autowired
-    private RedisConnectionFactory redisConnectionFactory;
+        @Autowired
+        private RedisConnectionFactory redisConnectionFactory;
 
-    @Bean
-    public CacheManager verifyCodeCacheManager() {
-        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
-        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        @Bean
+        public CacheManager verifyCodeCacheManager() {
+                RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+                Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
 
-        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration
-                .defaultCacheConfig()
-                .entryTtl(java.time.Duration.ofMinutes(5))
-                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer))
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer))
-                .disableCachingNullValues();;
+                RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration
+                                .defaultCacheConfig()
+                                .entryTtl(java.time.Duration.ofMinutes(5))
+                                .serializeKeysWith(RedisSerializationContext.SerializationPair
+                                                .fromSerializer(redisSerializer))
+                                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                                                .fromSerializer(jackson2JsonRedisSerializer))
+                                .disableCachingNullValues();
+                ;
 
-        return RedisCacheManager.builder(redisConnectionFactory)
-                .cacheDefaults(redisCacheConfiguration)
-                .build();
-    }
+                return RedisCacheManager.builder(redisConnectionFactory)
+                                .cacheDefaults(redisCacheConfiguration)
+                                .build();
+        }
 
-    @Bean
-    @Primary
-    public CacheManager userCacheManager(){
-        RedisSerializer<String> redisSerializer = new StringRedisSerializer();
-        Jackson2JsonRedisSerializer<User> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(User.class);
+        @Bean
+        @Primary
+        public CacheManager userCacheManager() {
+                RedisSerializer<String> redisSerializer = new StringRedisSerializer();
+                Jackson2JsonRedisSerializer<User> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(
+                                User.class);
 
-        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration
-                .defaultCacheConfig()
-                .entryTtl(java.time.Duration.ofMinutes(10))
-                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer))
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer))
-                .disableCachingNullValues();;
+                RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration
+                                .defaultCacheConfig()
+                                .entryTtl(java.time.Duration.ofMinutes(10))
+                                .serializeKeysWith(RedisSerializationContext.SerializationPair
+                                                .fromSerializer(redisSerializer))
+                                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                                                .fromSerializer(jackson2JsonRedisSerializer))
+                                .disableCachingNullValues();
+                ;
 
-        return RedisCacheManager.builder(redisConnectionFactory)
-                .cacheDefaults(redisCacheConfiguration)
-                .build();
-    }
+                return RedisCacheManager.builder(redisConnectionFactory)
+                                .cacheDefaults(redisCacheConfiguration)
+                                .build();
+        }
 }
